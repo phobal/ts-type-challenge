c6b82768c9b68163badc923143957036a45e33aa
phobal <phobal@126.com>
2021-09-29 17:54:03 +0800

    :rocket: 3062,Shift

---
+++ src/2-medium/3062-Shift.ts
@@ -0,0 +1,37 @@
+/*
+  3062 - Shift
+  -------
+  by jiangshan (@jiangshanmeta) #medium #array
+  
+  ### Question
+  
+  Implement the type version of ```Array.shift```
+  
+  For example
+  
+  ```typescript
+  type Result = Shift<[3, 2, 1]> // [2, 1]
+  ```
+  
+  > View on GitHub: https://tsch.js.org/3062
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// 类似 Pop https://tsch.js.org/15
+type Shift<T> = T extends [first: unknown, ...other: infer R] ? R : never;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect, ExpectFalse, NotEqual } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<["a", "b", "c", "d"]>, ["b", "c", "d"]>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3062/answer
+  > View solutions: https://tsch.js.org/3062/solutions
+  > More Challenges: https://tsch.js.org
+*/
